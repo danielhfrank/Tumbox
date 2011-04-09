5be3ee5ec1386c448061d0e51d69a789db7623c9
Dan Frank <danielhfrank@gmail.com>
2011-04-09 18:32:22 -0400

    example config

--- src/mbpconfig_example.py
+++ src/mbpconfig_example.py
@@ -7,26 +7,26 @@ Created on Apr 3, 2011
 '''
 
 #Music directory - where it all goes down
-musicdir = '/Users/dan/Dropbox/Music Butt Pirate'
+musicdir = ''
 
 #Skip Directories - list any subdirs that you don't want to follow.
 #DON'T write out absolute paths
-skip_dirs = ['DOCS - plz read', 'For Shame', '.frenzy']
+skip_dirs = []
 
 
 #Write to an archives folder?
 local_archive = True
 
 #Archive folder (absolute path!)
-archive_dir = '/Users/dan/Dropbox/Music Butt Pirate/DOCS - plz read/Archives'
+archive_dir = ''
 
 #Test archive folder (probably don't mess with this unless you are monkeying with code"
-archive_test_dir = '/Users/dan/Desktop/Archives'
+archive_test_dir = ''
 
 #Log file - log output will be stored here.. someday
-log_file = '/Users/dan/Dropbox/.mbp_helpers/log.txt'
+log_file = ''
 
 #DB file - if using a filesystem 'db'
-db_file = '/Users/dan/Dropbox/.mbp_helpers/mbp_db.json'
+db_file = ''
 
 
